9abcd9c56683fce0df9ae1880ea80ace78bbe267
Jerome Quelin <jquelin@cpan.org>
2003-02-22 11:25:01 +0000

    Import of JQUELIN/Language-Ook-0.02 from CPAN.
    gitpan-cpan-distribution: Language-Ook
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         JQUELIN/Language-Ook-0.02.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

--- Ook.pm
+++ Ook.pm
@@ -1,4 +1,4 @@
-# $Id: Ook.pm,v 1.1.1.1 2002/10/06 09:07:49 jquelin Exp $
+# $Id: Ook.pm,v 1.2 2003/02/22 10:32:39 jquelin Exp $
 #
 # Copyright (c) 2002 Jerome Quelin <jquelin@cpan.org>
 # All rights reserved.
@@ -9,7 +9,7 @@
 
 package Language::Ook;
 
-use v5.6;
+use 5.006;
 
 =head1 NAME
 
@@ -22,6 +22,9 @@ Language::Ook - a Ook! interpreter.
     my $interp = new Language::Ook( "program.ook" );
     $interp->run_code;
 
+    # Print the Perl code.
+    $interp->print_code;
+
 
 =head1 DESCRIPTION
 
@@ -33,26 +36,10 @@ Ook! is bijective with BrainFuck, and thus, Turing-complete.
 =cut
 
 
-#---------------#
-#   Includes.   #
-#---------------#
 use strict;
 use warnings;
 use Carp;
-
-#----------------#
-#   Variables.   #
-#----------------#
-our $VERSION = "0.01";
-our %method = 
-  ( "Ook. Ook?" => "mv_ptr_up",
-    "Ook? Ook." => "mv_ptr_down",
-    "Ook. Ook." => "inc_ptr",
-    "Ook! Ook!" => "dec_ptr",
-    "Ook. Ook!" => "read_cell",
-    "Ook! Ook." => "print_cell",
-    "Ook! Ook?" => "start_loop",
-    "Ook? Ook!" => "end_loop" );
+our $VERSION = "0.02";
 
 
 =head1 CONSTRUCTOR
@@ -60,32 +47,45 @@ our %method =
 =head2 new( [filename] )
 
 Create a new Ook interpreter. If a filename is provided, then read
-and store the content of the file in the cartesian Lahey space
-topology of the interpreter.
+and store the content of the file.
 
 =cut
 sub new {
     # Create and bless the object.
     my $proto = shift;
     my $class = ref($proto) || $proto;
-    my $self  = 
-      { cells => [0], # memory cells
-        ptr   => 0,   # pointer to current memory cell
-        code  => [],  # code to be executed
-        ip    => 0,   # instruction pointer
-      };
+    my $self  = { code => "" };
     bless $self, $class;
-
     # Read the file if needed.
     my $file = shift;
     defined($file) and $self->read_file( $file );
-
     # Return the object.
     return $self;
 }
 
 
-=head1 Public methods
+=head1 ACCESSORS
+
+=head2 code (  )
+
+Return the associated Perl code.
+
+=cut
+
+sub code :lvalue { $_[0]->{code} }
+
+
+=head1 PUBLIC METHODS
+
+=head2 print_code(  )
+
+Print the equivalent Perl code.
+
+=cut
+
+sub print_code {
+    print $_[0]->code;
+}
 
 =head2 read_file( filename )
 
@@ -97,212 +97,50 @@ Side effect: clear the previous code.
 
 sub read_file {
     my ($self, $filename) = @_;
-
     # Fetch the code.
     my $code;
     open OOK, "<$filename" or croak "$filename: $!";
     {
         local $/; # slurp mode.
         $code = <OOK>;
+        $code .= " ";
     }
     close OOK;
-
     # Store code.
-    $self->store_code( $code );
+    $code =~ s/[\n\s]+/ /g;
+    my $perl = "{ local \$^W = 0;\nuse bytes;\nmy \@cell = ();\nmy \$ptr = 0;\n";
+    while ( $code =~ m/Ook(.) Ook(.) /g ) {
+        my $instr = $1.$2;
+      sw: {
+            $instr eq ".?" and $perl .= '$ptr++;', last sw;
+            $instr eq "?." and $perl .= '$ptr--;', last sw;
+            $instr eq ".." and $perl .= '$cell[$ptr]++;', last sw;
+            $instr eq "!!" and $perl .= '$cell[$ptr]--;', last sw;
+            $instr eq ".!" and $perl .= 'read(STDIN,$cell[$ptr],1)?ord($cell[$ptr]):0;', last sw;
+            $instr eq "!." and $perl .= 'print chr($cell[$ptr]);', last sw;
+            $instr eq "!?" and $perl .= 'while ($cell[$ptr]) {', last sw;
+            $instr eq "?!" and $perl .= '}', last sw;
+            croak "Ook! Ook$1 Ook$2\n";
+        }
+        $perl .= "\n";
+    }
+    $perl .= "}\n";
+    $self->code = $perl;
 }
 
 
-=head2 run(  )
+=head2 run_code(  )
 
-Run the code stored.
+Run the stored code.
 
 =cut
 
 sub run_code {
     my $self = shift;
-
-    $self->{ptr} = 0;
-    $self->{ip} = 0;
-    while ( $self->{ip} < scalar( @{$self->{code}} ) ) {
-        $self->tick;
-    }
+    eval $self->{code};
 }
 
 
-=head2 store_code( code )
-
-Store the given code and check wether it is valid Ook! code.
-
-=cut
-
-sub store_code {
-    my ($self, $code) = @_;
-
-    my $depth = 0;
-
-    # Parse code.
-    $code =~ s/[\n\s]+/ /g;
-    while ( $code =~ /(Ook[.?!] Ook[.?!])/g ) {
-        exists $method{$1} or croak "Unknown Ook instruction '$1'";
-        $depth++ if $1 eq "Ook! Ook?";
-        $depth-- if $1 eq "Ook? Ook!";
-        $depth < 0 and croak "Unmatched 'Ook? Ook!' instruction";
-        push @{$self->{code}}, $1;  # store code.
-    }
-
-    # Sanity check.
-    $depth != 0 and croak "Unmatched 'Ook! Ook?' instruction";
-    $code =~ /(?<!Ook[?.!] )Ook[?.!] (?!Ook[?.!])/ and croak "Syntax Ook error";
-}
-
-
-=head2 tick(  )
-
-Execute the next Ook instruction to be executed.
-
-=cut
-
-sub tick {
-    my $self = shift;
-    my $meth = $method{ $self->{code}[$self->{ip}] };
-    $self->$meth;
-    $self->{ip}++;
-}
-
-=head1 Private methods
-
-Those methods should B<not> be called directly. Use at your own risks,
-you've been warned!
-
-
-=head2 dec_ptr(  )
-
-Implement the C<Ook! Ook!> instruction.
-
-=cut
-
-sub dec_ptr {
-    my $self = shift;
-    $self->{cells}[$self->{ptr}]--;
-}
-
-
-=head2 end_loop(  )
-
-Implement the C<Ook? Ook!> instruction.
-
-=cut
-
-sub end_loop {
-    my $self = shift;
-
-    # The current cell is null, so don't jump back.
-    return if $self->{cells}[$self->{ptr}] == 0;
-
-    # Cell is not null, so jump back to the beginning of the loop.
-    my $i = $self->{ip};
-    my $depth = 1;
-
-    do {
-        $i--;
-        $depth++ if $self->{code}[$i] eq "Ook? Ook!";
-        $depth-- if $self->{code}[$i] eq "Ook! Ook?";
-    } until ( $depth == 0 );
-    $self->{ip} = $i;
-}
-
-
-=head2 inc_ptr(  )
-
-Implement the C<Ook. Ook.> instruction.
-
-=cut
-
-sub inc_ptr {
-    my $self = shift;
-    $self->{cells}[$self->{ptr}]++;
-}
-
-
-=head2 mv_ptr_down(  )
-
-Implement the C<Ook? Ook.> instruction.
-
-=cut
-
-sub mv_ptr_down {
-    my $self = shift;
-
-    $self->{ptr}--;
-    croak "Can't go beyond 0th element" if $self->{ptr} < 0;
-}
-
-
-=head2 mv_ptr_up(  )
-
-Implement the C<Ook. Ook?> instruction.
-Append new cells if the current cells aren't enough.
-
-=cut
-
-sub mv_ptr_up {
-    my $self = shift;
-
-    $self->{ptr}++;
-    if ( scalar(@{$self->{cells}}) <= $self->{ptr} ) {
-        push @{$self->{cells}}, (0)x 1024;
-    }
-}
-
-
-=head2 print_cell(  )
-
-Implement the C<Ook! Ook.> instruction.
-
-=cut
-
-sub print_cell {
-    my $self = shift;
-    print chr($self->{cells}[$self->{ptr}]);
-}
-
-
-=head2 read_cell(  )
-
-Implement the C<Ook. Ook!> instruction.
-
-=cut
-
-sub read_cell {
-    my $self = shift;
-    $/ = \1;  # read only one char.
-    $self->{cells}[$self->{ptr}] = ord( <STDIN> );
-}
-
-
-=head2 start_loop(  )
-
-Implement the C<Ook! Ook?> instruction.
-
-=cut
-
-sub start_loop {
-    my $self = shift;
-
-    # The current cell is not null, so don't jump.
-    return if $self->{cells}[$self->{ptr}] != 0;
-
-    # Uh, cell is null: jump to end of loop.
-    my $i = $self->{ip};
-    my $depth = 1;
-
-    do {
-        $i++;
-        $depth++ if $self->{code}[$i] eq "Ook! Ook?";
-        $depth-- if $self->{code}[$i] eq "Ook? Ook!";
-    } until ( $depth == 0 );
-    $self->{ip} = $i;
-}
 
 1;
 __END__
--- t/ook.t
+++ t/ook.t
@@ -1,57 +1,35 @@
 #-*- cperl -*-
-# $Id: ook.t,v 1.1.1.1 2002/10/06 09:07:49 jquelin Exp $
+# $Id: ook.t,v 1.2 2003/02/22 11:15:00 jquelin Exp $
 #
 
 use strict;
 use Language::Ook;
 use POSIX qw! tmpnam !;
 use Test;
-BEGIN { plan tests => 6 };
+BEGIN { plan tests => 2 };
 
 # 
 
-# Sanity checks.
-eval { 
-    my $interp = new Language::Ook;
-    $interp->store_code( "Ook? Ook?" );
-};
-ok( $@, qr/^Unknown Ook instruction 'Ook\? Ook\?'/ );
-eval { 
-    my $interp = new Language::Ook;
-    $interp->store_code( "Ook? blah blah Ook!" );
-};
-ok( $@, qr/^Syntax Ook error/ );
-eval { 
-    my $interp = new Language::Ook;
-    $interp->store_code( "Ook! Ook?" );
-};
-ok( $@, qr/^Unmatched 'Ook! Ook\?'/ );
-eval {
-    my $interp = new Language::Ook;
-    $interp->store_code( "Ook? Ook!" );
-};
-ok( $@, qr/^Unmatched 'Ook\? Ook!'/ );
-eval {
-    my $interp = new Language::Ook;
-    $interp->store_code( "Ook? Ook! Ook! Ook?" );
-};
-ok( $@, qr/^Unmatched 'Ook\? Ook!'/ );
-
 # Classic hello world.
-my $file = tmpnam();
-open OUT, ">$file" or die $!;
-my $fh = select OUT;
-my $interp = new Language::Ook;
-$interp->read_file( "hello.ook" );
-$interp->run_code;
-select $fh;
-close OUT;
-open OUT, "<$file" or die $!;
-my $content;
-{
-    local $/;
-    $content = <OUT>;
+my %tests = ( "hello.ook" =>  "Hello, world!\n",
+              "test.ook"  =>  "1..1\nok 1\n",
+            );
+for my $f ( sort keys %tests ) {
+    my $file = tmpnam();
+    open OUT, ">$file" or die $!;
+    my $fh = select OUT;
+    my $interp = new Language::Ook;
+    $interp->read_file( $f );
+    $interp->run_code;
+    select $fh;
+    close OUT;
+    open OUT, "<$file" or die $!;
+    my $content;
+    {
+        local $/;
+        $content = <OUT>;
+    }
+    close OUT;
+    unlink $file;
+    ok( $content, $tests{$f} );
 }
-close OUT;
-unlink $file;
-ok( $content, "Hello, world!\n");
