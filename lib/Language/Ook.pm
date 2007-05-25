14fbd4f108224ebacd2654ab8327b4d6f33a0f64
Jerome Quelin <jquelin@cpan.org>
2007-05-25 07:58:18 +0000

    Import of JQUELIN/Language-Ook-1.0.0 from CPAN.
    gitpan-cpan-distribution: Language-Ook
    gitpan-cpan-version:      1.0.0
    gitpan-cpan-path:         JQUELIN/Language-Ook-1.0.0.tar.gz
    gitpan-cpan-author:       JQUELIN
    gitpan-cpan-maturity:     released

--- lib/Language/Ook.pm
+++ lib/Language/Ook.pm
@@ -1,10 +1,21 @@
-# $Id: Ook.pm,v 1.3 2003/02/22 19:08:14 jquelin Exp $
 #
-# Copyright (c) 2002 Jerome Quelin <jquelin@cpan.org>
-# All rights reserved.
+# This file is part of Language::Ook.
+# Copyright (c) 2002-2007 Jerome Quelin, all rights reserved.
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the same terms as Perl itself.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or (at
+# your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+#
 #
 
 package Language::Ook;
@@ -39,7 +50,7 @@ Ook! is bijective with BrainFuck, and thus, Turing-complete.
 use strict;
 use warnings;
 use Carp;
-our $VERSION = "0.03";
+our $VERSION = '1.0.0';
 
 
 =head1 CONSTRUCTOR
--- t/02-pod-coverage.t
+++ t/02-pod-coverage.t
@@ -19,35 +19,7 @@
 #
 #
 
-use strict;
-use File::Spec::Functions;
-use Language::Ook;
-use POSIX qw! tmpnam !;
-use Test;
-BEGIN { plan tests => 2 };
-
-#
-
-# Classic hello world.
-my %tests = ( "hello.ook" =>  "Hello, world!\n",
-              "test.ook"  =>  "1..1\nok 1\n",
-            );
-for my $f ( sort keys %tests ) {
-    my $file = tmpnam();
-    open OUT, ">$file" or die $!;
-    my $fh = select OUT;
-    my $interp = new Language::Ook;
-    $interp->read_file( catfile( "examples", $f ) );
-    $interp->run_code;
-    select $fh;
-    close OUT;
-    open OUT, "<$file" or die $!;
-    my $content;
-    {
-        local $/;
-        $content = <OUT>;
-    }
-    close OUT;
-    unlink $file;
-    ok( $content, $tests{$f} );
-}
+use Test::More;
+eval "use Test::Pod::Coverage 1.04";
+plan skip_all => "Test::Pod::Coverage 1.04 required for testing POD coverage" if $@;
+all_pod_coverage_ok();
